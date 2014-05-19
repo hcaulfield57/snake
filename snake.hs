5d3f8fbe2434df3c618b4efad1a1bf3b96c3c258
Grant Mather <hcaulfield57@gmail.com>
2014-05-18 18:23:05 -0600

    more work

--- snake.hs
+++ snake.hs
@@ -11,12 +11,17 @@ import System.Random
 data Direction = UP | DOWN | LEFT | RIGHT
     deriving Eq
 
-newtype Point = Pt { getPt :: (GLfloat,GLfloat) }
-data Rectangle = Rect { minPt, maxPt :: Point }
-type Food = (Rectangle,StdGen)
-type Snake = [Rectangle] 
+newtype Point = Pt { getPt :: (GLfloat, GLfloat) }
+    deriving Show -- DEBUG
 
-type SnakeInfo = (IORef Snake, IORef Direction, IORef Food)
+data Rectangle = Rect { minPt, maxPt :: Point }
+    deriving Show -- DEBUG
+
+type Food = (Rectangle, StdGen)
+
+type Snake = [Rectangle]
+
+type GameInfo = (IORef Snake, IORef Direction, IORef Food)
 
 snakeColor :: Color4 GLfloat
 snakeColor = Color4 0.506 0.928 0.4 0
@@ -32,107 +37,152 @@ foodSize = 0.05
 
 main :: IO ()
 main = do
-    _ <- getArgsAndInitialize
-    _ <- createWindow "Snake"
-    snakeRef <- newIORef [(Rect(Pt(0,0)) (Pt(snakeSize,snakeSize)))]
-    dirRef <- newIORef RIGHT
-    foodRef <- newIORef ((Rect(Pt(0.0,0.5)) (Pt(0+foodSize,0.5+foodSize))),mkStdGen 0)
-    windowSize $= Size 300 300
-    idleCallback $= Just (snakeLoop (snakeRef,dirRef,foodRef))
+    _        <- getArgsAndInitialize
+    _        <- createWindow "Snake"
+    snakeRef <- newIORef 
+        [Rect (Pt (0, 0)) (Pt (snakeSize, snakeSize))]
+    dirRef   <- newIORef RIGHT
+    foodRef  <- newIORef
+        (Rect (Pt (0.0, 0.5)) (Pt (foodSize, 0.5+foodSize)), mkStdGen 0)
+    windowSize            $= Size 300 300
+    displayCallback       $= initGame
+    idleCallback          $= Just (gameLoop (snakeRef, dirRef, foodRef))
     keyboardMouseCallback $= Just (handleKeys dirRef)
     mainLoop
 
-snakeLoop :: SnakeInfo -> DisplayCallback
-snakeLoop (snakeRef,dirRef,foodRef) = do
+initGame :: DisplayCallback
+initGame = do
+    clearColor $= Color4 1 1 1 0
+    clear [ColorBuffer]
+
+gameLoop :: GameInfo -> DisplayCallback
+gameLoop gameInfo@(snakeRef, dirRef, foodRef) = do
     snake <- readIORef snakeRef
-    dir <- readIORef dirRef
-    food <- readIORef foodRef
+    food  <- readIORef foodRef
     clearColor $= Color4 1 1 1 0
     clear [ColorBuffer]
     draw foodColor (fst food)
     mapM_ (draw snakeColor) snake
+    -- DEBUG
+    printDebug snake food
+    -- END DEBUG
     flush
-    checkCollision (snakeRef,dirRef,foodRef)
-    let newHead = moveHead dir (head snake)
-    writeIORef snakeRef (moveSnake newHead snake)
+    checkCollision gameInfo
     threadDelay 250000
 
-moveHead :: Direction -> Rectangle -> Rectangle
-moveHead dir r = 
-    let (minx,miny) = getPt . minPt $ r
-        (maxx,maxy) = getPt . maxPt $ r
-    in case dir of
-        LEFT -> Rect(Pt(minx-snakeSize,miny)) (Pt(maxx-snakeSize,maxy))
-        UP -> Rect(Pt(minx,miny+snakeSize)) (Pt(maxx,maxy+snakeSize))
-        DOWN -> Rect(Pt(minx,miny-snakeSize)) (Pt(maxx,maxy-snakeSize))
-        RIGHT -> Rect(Pt(minx+snakeSize,miny)) (Pt(maxx+snakeSize,maxy))
+printDebug snake food = do
+    putStrLn $ "Food = " ++ show food
+    putStrLn $ "Snake = " ++ show snake
 
-moveSnake :: Rectangle -> Snake -> Snake
-moveSnake _ [] = []
-moveSnake r (s:ss) = r : moveSnake s ss
+handleKeys :: IORef Direction -> KeyboardMouseCallback
+handleKeys keyRef (Char k) Down _ _ =
+    unless (k `notElem` "hjkl") $ 
+        let newDir = case k of
+                'h' -> LEFT
+                'l' -> RIGHT
+                'j' -> DOWN
+                'k' -> UP
+        in writeIORef keyRef newDir
+handleKeys _ (Char 'q') _ _ _ = exitWith ExitSuccess
+handleKeys _ _ _ _ _ = return ()
 
 draw :: Color4 GLfloat -> Rectangle -> IO ()
-draw c r = do
-    let (minx,miny) = getPt . minPt $ r
-        (maxx,maxy) = getPt . maxPt $ r
+draw col rect = do
+    let (minx,miny) = getPt . minPt $ rect
+        (maxx,maxy) = getPt . maxPt $ rect
     renderPrimitive Quads $ do
-        color c
+        color col
         vertex $ Vertex2 minx miny
         vertex $ Vertex2 maxx miny
         vertex $ Vertex2 maxx maxy
         vertex $ Vertex2 minx maxy
 
-handleKeys :: IORef Direction -> KeyboardMouseCallback
-handleKeys keyRef (Char k) Down _ _ = unless
-    (k `notElem` "hjkl") $ 
-        let newDir = case k of
-             'h' -> LEFT
-             'j' -> DOWN
-             'k' -> UP
-             'l' -> RIGHT
-        in writeIORef keyRef newDir
-handleKeys _ (Char 'q') _ _ _ = exitWith ExitSuccess
-handleKeys _ _ _ _ _ = return ()
-
-spawnFood :: Food -> Food
-spawnFood (r,gen) = 
-    let (pt,gen') = random gen :: (Integer,StdGen)
-        loc = CFloat (fromIntegral (pt `mod` 1)) :: GLfloat
-    in ((Rect(Pt(loc,loc)) (Pt(loc+foodSize,loc+foodSize))),gen')
-
-checkCollision :: SnakeInfo -> IO ()
-checkCollision (snakeRef,dirRef,foodRef) = do
+checkCollision :: GameInfo -> IO ()
+checkCollision (snakeRef, dirRef, foodRef) = do
     snake <- readIORef snakeRef
-    dir <- readIORef dirRef
-    food <- readIORef foodRef
-    let (sminx,_) = getPt . minPt $ head snake
-        (_,smaxy) = getPt . maxPt $ head snake
-    when ((fst food) `inRect` (head snake)) $ do
+    dir   <- readIORef dirRef
+    food  <- readIORef foodRef
+    -- ate food
+    when (foodCollision (fst food) (head snake)) $ do
+        -- DEBUG
+        putStrLn $ "COLLISION AT: Food = " ++ show food ++ " Snake = " ++ show snake
+        -- END DEBUG
         addSnake snakeRef dirRef
-        writeIORef foodRef (spawnFood food)
-    if sminx < (-1) || sminx > 1 || smaxy < -1 || smaxy > 1
-        then exitWith ExitSuccess
-        else return ()
+        spawnFood foodRef
+    -- hit wall
+    when (wallCollision (head snake)) $ do
+        exitWith ExitSuccess
+    -- hit self
+    when (selfCollision (head snake) (tail snake)) $ do
+        exitWith ExitSuccess
+    let newHead = moveHead (head snake) dir
+    writeIORef snakeRef (moveSnake newHead snake)
 
-inRect :: Rectangle -> Rectangle -> Bool
-inRect r1 r2 = 
-    let (minx1,_) = getPt . minPt $ r1
-        (_,maxy1) = getPt . minPt $ r1
-        (minx2,_) = getPt . minPt $ r2
-        (_,maxy2) = getPt . minPt $ r2
-    in if minx1 <= minx2 && maxy1 <= maxy2
-       then True 
+moveHead :: Rectangle -> Direction -> Rectangle
+moveHead rect dir =
+    let (minx,miny) = getPt . minPt $ rect
+        (maxx,maxy) = getPt . maxPt $ rect
+    in case dir of
+        LEFT  -> Rect (Pt (minx-snakeSize,miny)) (Pt (maxx-snakeSize,maxy))
+        RIGHT -> Rect (Pt (minx+snakeSize,miny)) (Pt (maxx+snakeSize,maxy))
+        DOWN  -> Rect (Pt (minx,miny-snakeSize)) (Pt (maxx,maxy-snakeSize))
+        UP    -> Rect (Pt (minx,miny+snakeSize)) (Pt (maxx,maxy+snakeSize))
+
+moveSnake :: Rectangle -> Snake -> Snake
+moveSnake _ [] = []
+moveSnake rect (s:ss) = rect : moveSnake s ss
+
+foodCollision :: Rectangle -> Rectangle -> Bool
+foodCollision food rect = 
+    let (minxf,_) = getPt . minPt $ food
+        (_,maxyf) = getPt . maxPt $ food
+        (minxs,_) = getPt . minPt $ rect
+        (_,maxys) = getPt . maxPt $ rect
+    in if minxf <= minxs && maxyf <= maxys
+       then True
        else False
 
 addSnake :: IORef Snake -> IORef Direction -> IO ()
 addSnake snakeRef dirRef = do
     snake <- readIORef snakeRef
-    dir <- readIORef dirRef
+    dir   <- readIORef dirRef
     let (minx,miny) = getPt . minPt $ head snake
         (maxx,maxy) = getPt . maxPt $ head snake
+        -- add new head to snake
         newSnake = case dir of
-         LEFT -> (Rect(Pt(minx,miny-snakeSize)) (Pt(maxx,maxy-snakeSize))) : snake
-         UP -> (Rect(Pt(minx+snakeSize,miny)) (Pt(maxx+snakeSize,maxy))) : snake
-         DOWN -> (Rect(Pt(minx-snakeSize,miny)) (Pt(maxx-snakeSize,maxy))) : snake
-         RIGHT -> (Rect(Pt(minx,miny-snakeSize)) (Pt(maxx,maxy-snakeSize))) : snake
+            LEFT  -> (Rect (Pt (minx-snakeSize,miny)) (Pt (maxx-snakeSize,maxy)))
+                : snake
+            RIGHT -> (Rect (Pt (minx+snakeSize,miny)) (Pt (maxx+snakeSize,maxy)))
+                : snake
+            UP    -> (Rect (Pt (minx,miny+snakeSize)) (Pt (maxx,maxy+snakeSize)))
+                : snake
+            DOWN  -> (Rect (Pt (minx,miny-snakeSize)) (Pt (minx,maxy-snakeSize)))
+                : snake
     writeIORef snakeRef newSnake
+
+spawnFood :: IORef Food -> IO ()
+spawnFood foodRef = do
+    (pt,gen) <- readIORef foodRef
+    let (pt',gen') = random gen :: (Integer,StdGen)
+        newPoint   = CFloat (fromIntegral (pt' `mod` 1)) :: GLfloat
+    writeIORef foodRef (Rect (Pt (newPoint,newPoint)) (Pt (newPoint+foodSize
+        , newPoint+foodSize)),gen')
+
+wallCollision :: Rectangle -> Bool
+wallCollision rect = 
+    let (minx,_) = getPt . minPt $ rect
+        (_,maxy) = getPt . maxPt $ rect
+    in if minx < (-1) || minx > 1 || maxy < (-1) || maxy > 1
+       then True
+       else False
+
+selfCollision :: Rectangle -> Snake -> Bool
+selfCollision _ [] = False
+selfCollision rect snake = 
+    let (hminx,_) = getPt . minPt $ rect
+        (_,hmaxy) = getPt . maxPt $ rect
+        (tminx,_) = getPt . minPt $ (head snake)
+        (_,tmaxy) = getPt . minPt $ (head snake)
+    in if hminx == tminx && hmaxy == tmaxy
+       then True
+       else selfCollision rect (tail snake)
